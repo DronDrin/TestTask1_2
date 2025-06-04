519d0c041ec9a2c9fb5a95dad0cb2684a060a73b
DronDrin <FunnyFunC@yandex.ru>
2025-06-04 06:40:32 +0300

    #2 / Make home page

--- src/main/resources/static/home.jsp
+++ src/main/resources/static/home.jsp
@@ -1,21 +1,42 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="UTF-8" />
-    <title>First JSP App</title>
+    <meta charset="UTF-8">
+    <title>Upload file</title>
     <script src="${pageContext.request.contextPath}/static/js/home-scripts.js"></script>
+    <link rel="stylesheet" href="${pageContext.request.contextPath}/static/css/home-styles.css">
 </head>
 <body>
-<input type="file" id="userFile">
-<button id="sendButton">send</button>
-<span id="progressBar"></span>
-<form action="api/v1/file/" method="get">
-    <input type="tel" name="id" id="id">
-    <button type="submit">download by id</button>
-</form>
-<form action="api/v1/file/remove/" method="post">
-    <input type="tel" name="id" id="id">
-    <button type="submit">delete by id</button>
-</form>
+<main class="main">
+    <div class="upload-form">
+        <h3 class="upload-form__header">Upload your file:</h3>
+        <div class="upload-form__file-area" role="button" id="uploadArea">
+            <div class="upload-form__file-area__text">Click to choose file, or drop it here</div>
+        </div>
+        <div class="progress-bar upload-form__progress-bar" id="uploadProgressBar">
+            <div class="progress-bar__wrapper">
+                <div class="progress-bar__body">
+                    <div class="progress-bar__indicator" id="progressBarIndicator"></div>
+                </div>
+                <div class="progress-bar__caption">
+                    <div class="progress-bar__text" id="progressBarText">Upload: 40%</div>
+                </div>
+            </div>
+        </div>
+        <div class="button upload-form__upload-button" id="uploadButton">Upload</div>
+        <div class="upload-form__finish">
+            <div class="upload-form__link-description">
+                Thanks! Your file was uploaded. Here's your link:
+            </div>
+            <div class="upload-form__link" id="fileLink">
+
+            </div>
+            <div class="button upload-form__another-button" id="uploadAnotherButton">
+                Upload another file
+            </div>
+        </div>
+    </div>
+</main>
+<input type="file" style="display: none" id="userFile">
 </body>
 </html>
--- src/main/resources/static/js/home-scripts.js
+++ src/main/resources/static/js/home-scripts.js
@@ -1,13 +1,38 @@
-let sendButton, userFile, progressBar;
+let uploadButton, uploadArea, progressBar, progressBarIndicator, progressBarText, userFileEl, uploadForm,
+    uploadAnotherButton, fileLinkEl;
+let _stage = 0;
 
 window.addEventListener('load', () => {
-    sendButton = document.getElementById("sendButton");
-    userFile = document.getElementById("userFile");
-    progressBar = document.getElementById("progressBar");
+    uploadButton = document.getElementById("uploadButton");
+    uploadArea = document.getElementById("uploadArea");
+    progressBar = document.getElementById("uploadProgressBar");
+    progressBarIndicator = document.getElementById("progressBarIndicator");
+    progressBarText = document.getElementById("progressBarText");
+    userFileEl = document.getElementById('userFile');
+    uploadAnotherButton = document.getElementById('uploadAnotherButton');
+    fileLinkEl = document.getElementById('fileLink');
+    uploadForm = document.querySelector('.upload-form');
 
-    sendButton.addEventListener('click', () => {
-        if (userFile.files.length > 0) {
-            const file = userFile.files[0];
+    uploadArea.addEventListener('click', () => {
+        if (getStage() <= 1)
+            userFileEl.click();
+    });
+
+    userFileEl.addEventListener('change', () => {
+        if (getStage() > 1)
+            return;
+        if (userFileEl.files.length > 0) {
+            uploadArea.innerText = userFileEl.files[0].name;
+            nextStage();
+        } else {
+            uploadArea.innerText = 'Click to choose file, or drop it here';
+        }
+    });
+
+    uploadButton.addEventListener('click', () => {
+        if (getStage() === 1) {
+            setStage(2);
+            const file = userFileEl.files[0];
             const request = new XMLHttpRequest();
             request.open("post", "/api/v1/file/");
             request.upload.addEventListener('progress', e => {
@@ -17,19 +42,89 @@ window.addEventListener('load', () => {
                     setProgressBarState(progressBar, undefined, e.loaded);
                 }
             });
-            request.responseType = "blob";
+            request.responseType = "text";
             request.upload.addEventListener('load', () => {
-                setProgressBarState(progressBar, 1)
+                setProgressBarState(progressBar, 1);
+            });
+            request.addEventListener('load', () => {
+                fileLinkEl.innerText = window.location.origin + '/download/?id=' + request.responseText;
+                nextStage();
             });
             const data = new FormData();
             data.set("userFile", file);
             request.send(data);
         }
     });
+
+    uploadAnotherButton.addEventListener('click', () => {
+        if (getStage() === 3)
+            resetStage();
+    });
 });
 
-
-function setProgressBarState(progressBar, decimalProgress, bytes) {
-    progressBar.innerText = decimalProgress === undefined ? bytes + " bytes" : Math.round(decimalProgress * 10000) / 100 + "%";
+function nextStage() {
+    if (_stage < 3)
+        setStage(_stage + 1);
 }
 
+function resetStage() {
+    setStage(0);
+}
+
+function setStage(newStage) {
+    _stage = newStage;
+    removeClass(uploadForm, 'upload-form_file-chosen');
+    removeClass(uploadForm, 'upload-form_uploading');
+    removeClass(uploadForm, 'upload-form_finish');
+    switch (_stage) {
+        case 1:
+            addClass(uploadForm, 'upload-form_file-chosen');
+            break;
+        case 2:
+            addClass(uploadForm, 'upload-form_uploading');
+            break;
+        case 3:
+            addClass(uploadForm, 'upload-form_finish');
+            break;
+    }
+}
+
+function getStage() {
+    return _stage;
+}
+
+function setProgressBarState(progressBar, decimalProgress, bytes) {
+    if (decimalProgress === undefined) {
+        addClass(progressBar, 'progress-bar_unset')
+        if (bytes < 10000) {
+            progressBarText.innerText = bytes + ' bytes';
+        } else if (bytes < 1000000) {
+            progressBarText.innerText = round(bytes / 1024, 2) + ' KiB';
+        } else if (bytes < 1000000000) {
+            progressBarText.innerText = round(bytes / 1048576, 2) + ' MiB';
+        } else {
+            progressBarText.innerText = round(bytes / 1073741824, 2) + ' GiB';
+        }
+
+    } else {
+        removeClass(progressBar, 'progress-bar_unset');
+        const percentage = round(decimalProgress * 100, 2) + '%';
+        progressBarIndicator.style.width = percentage;
+        progressBarText.innerText = percentage;
+    }
+}
+
+function addClass(element, className) {
+    if (!element.classList.contains(className))
+        element.classList.add(className);
+}
+
+function removeClass(element, className) {
+    if (element.classList.contains(className))
+        element.classList.remove(className);
+}
+
+function round(number, fractionalPartSize) {
+    const multiplier = Math.pow(10, fractionalPartSize);
+    return Math.round(number * multiplier) / multiplier;
+}
